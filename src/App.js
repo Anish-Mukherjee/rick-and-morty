172b357de077b74b0d9aa23693202bc2afc9d0ee
Anish Mukherjee <cybertronian217@gmail.com>
2022-02-02 12:28:06 +0530

    Setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = () => {
+
+    const article = (
+        <article className="mx-auto max-w-sm shadow-xl bg-cover bg-center min-h-150 transform duration-500 hover:-translate-y-2 cursor-pointer group" style={{ backgroundImage: `url("https://images.pexels.com/photos/3299386/pexels-photo-3299386.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=900")` }}>
+            <div className="bg-black bg-opacity-20 min-h-150 px-10 flex flex-wrap flex-col pt-96 hover:bg-opacity-75 transform duration-300">
+                <h1 className="text-white text-3xl mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300">
+                    On A Day Like Today
+                </h1>
+                <div className="w-16 h-2 bg-yellow-500 rounded-full mb-5 transform translate-y-20 group-hover:translate-y-0 duration-300">
+                </div>
+                <p className="opacity-0 text-white text-xl group-hover:opacity-80 transform duration-500">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, beatae!
+                </p>
+            </div>
+        </article>
+    )
+
+    return (
+        <div>
+            <h1 className="text-3xl font-bold underline">
+                Rick and Morty!
+            </h1>
+        </div>
+    )
 }
 
-export default App;
+export default App
